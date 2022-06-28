d084ebcbb73d0745614bc5e4b6139087945d2d73
izpakla <dulepn88@gmail.com>
2022-06-28 21:35:29 +0200

    Fixed publish configuration

--- app/src/main/java/rs/rocketbyte/limbo/MainActivity.kt
+++ app/src/main/java/rs/rocketbyte/limbo/MainActivity.kt
@@ -3,10 +3,13 @@ package rs.rocketbyte.limbo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import rs.rocketbyte.handyextensions.R
+import rs.rocketbyte.limbo.ext.isLimbo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        isLimbo()
     }
 }
